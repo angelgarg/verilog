8d96ce0c809c05c877e3fff6788a9f94a0995358
angelgarg <145918491+angelgarg@users.noreply.github.com>
2024-10-12 01:23:59 +0530

    Add files via upload

--- assignment-1/ass1.v
+++ assignment-1/ass1.v
@@ -1,1 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/12/2024 01:15:48 AM
+// Design Name: 
+// Module Name: ass1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module ass1();
+initial begin
+$display("My name is Angel Garg and I start the course on 11 oct 2024.");
+end
+endmodule
